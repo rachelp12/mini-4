6ed573cf71a3b9ab05b675d87d34c4ce8cfe9d6d
ruiqi <rs858@njit.edu>
2018-04-21 14:05:35 -0400

    Added the view question link in hompage view. Added question page view and Controller

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -13,11 +13,13 @@ class AnswersTableSeeder extends Seeder
     {
         $users = App\User::inRandomOrder();
         $users->each(function ($user){
-            $question = App\Question::inRandomOrder()->first();
-            $answer = factory(\App\Answer::class)->make();
-            $answer->user()->associate($user);
-            $answer->question()->associate($question);
-            $answer->save();
+            for($i = 1; $i < 6; $i++) {
+                $question = App\Question::inRandomOrder()->first();
+                $answer = factory(\App\Answer::class)->make();
+                $answer->user()->associate($user);
+                $answer->question()->associate($question);
+                $answer->save();
+            }
         });
     }
 }
