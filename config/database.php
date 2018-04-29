8a34d2e4e24abb6f19d66bcc617e8c8d45516d9b
ruiqi <rs858@njit.edu>
2018-04-29 13:12:45 -0400

    created branch, hide heroku database settings temporarily

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+//$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
 
 return [
 
@@ -80,16 +80,16 @@ return [
             'prefix' => '',
         ],
 
-        'pg-heroku' => [
-            'driver' => 'pgsql',
-            'host' => $heroku_db_url['host'],
-            'database' => substr($heroku_db_url['path'], 1),
-            'username' => $heroku_db_url['user'],
-            'password' => $heroku_db_url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+//        'pg-heroku' => [
+//            'driver' => 'pgsql',
+//            'host' => $heroku_db_url['host'],
+//            'database' => substr($heroku_db_url['path'], 1),
+//            'username' => $heroku_db_url['user'],
+//            'password' => $heroku_db_url['pass'],
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//        ],
 
     ],
 
