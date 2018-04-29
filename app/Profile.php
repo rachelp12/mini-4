a744526b7fa750ca6b34759252ad438c1154015e
ruiqi <rs858@njit.edu>
2018-04-29 14:04:19 -0400

    Build create file page, add error, message, status pages to view

--- app/Profile.php
+++ app/Profile.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
+
 
 class Profile extends Model
 {
+    protected $fillable = ['fname', 'lname', 'body'];
     //
     public function user()
     {
